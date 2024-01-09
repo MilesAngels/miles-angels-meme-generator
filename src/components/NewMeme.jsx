57d10c53c92218e0a61491739ad2cd3321c58c6e
Miles Angels <milesmarieangels@gmail.com>
2024-01-09 15:27:04 -0800

    update state for topText and bottomText

--- src/components/NewMeme.jsx
+++ src/components/NewMeme.jsx
@@ -1,46 +1,68 @@
-import memesData from "../memesData"
-import {useState} from "react"
+import memesData from "../memesData";
+import { useState } from "react";
 
 export default function NewMeme() {
-    const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        randomImg: ""
-    });
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImg: "",
+  });
 
-    const [allMemeImg, setAllMemeImg] = useState(memesData);
-    
-    // Get new Meme Image
-    function getMemeImg() {
-        const memesArray = allMemeImg.data.memes;
-        const randomNum = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomNum].url;
-        setMeme( prevMeme => ({
-            ...prevMeme,
-            randomImg: url
-        }))
+  const [allMemeImg, setAllMemeImg] = useState(memesData);
 
-    }    
+  // Get new Meme Image
+  function getMemeImg() {
+    const memesArray = allMemeImg.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNum].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImg: url,
+    }));
+  }
 
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+    }))
+  }
 
-    return (
-        <main>
-            <div className="new-meme-container">
-                <div className="input1">
-                    <label className="first-label">Top text</label>
-                    <input className="first-text medium-font" type="text"/>
-                </div>
-                
-                <div className="input2">
-                    <label className="second-label">Bottom Text</label>
-                    <input className="second-text medium-font" type="text"/>
-                </div>
-                
-                <button className="submit-btn" type="button" onClick={getMemeImg}>Generate Another Meme 🖼</button>
-            </div>
-            <div className="meme-img">
-                <img src={meme.randomImg}/>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="new-meme-container">
+        <div className="input1">
+          <label className="first-label">Top text</label>
+          <input
+            className="first-text medium-font"
+            type="text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="input2">
+          <label className="second-label">Bottom Text</label>
+          <input
+            className="second-text medium-font"
+            type="text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button className="submit-btn" type="button" onClick={getMemeImg}>
+          Generate Another Meme 🖼
+        </button>
+      </div>
+      <div className="meme-img">
+        <img src={meme.randomImg} />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
 }
